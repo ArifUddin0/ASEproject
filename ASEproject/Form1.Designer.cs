dd2530e5c515ffb76b7de6c09d1cd3df94443d36
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-10-27 16:48:50 +0100

    Panels/buttons added to the GUI and save button is working

--- ASEproject/Form1.Designer.cs
+++ ASEproject/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ASEproject
+﻿using System;
+
+namespace ASEproject
 {
     partial class Form1
     {
@@ -28,13 +30,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.graphicPanel = new System.Windows.Forms.Panel();
+            this.commandPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(26, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(135, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Syntax";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // textBoxCommand
+            // 
+            this.textBoxCommand.Location = new System.Drawing.Point(26, 248);
+            this.textBoxCommand.Name = "textBoxCommand";
+            this.textBoxCommand.Size = new System.Drawing.Size(325, 20);
+            this.textBoxCommand.TabIndex = 2;
+            this.textBoxCommand.TextChanged += new System.EventHandler(this.textBoxCommand_TextChanged);
+            // 
+            // graphicPanel
+            // 
+            this.graphicPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.graphicPanel.Location = new System.Drawing.Point(465, 12);
+            this.graphicPanel.Name = "graphicPanel";
+            this.graphicPanel.Size = new System.Drawing.Size(324, 216);
+            this.graphicPanel.TabIndex = 3;
+            this.graphicPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.graphicPanel_Paint);
+            // 
+            // commandPanel
+            // 
+            this.commandPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.commandPanel.Location = new System.Drawing.Point(26, 12);
+            this.commandPanel.Name = "commandPanel";
+            this.commandPanel.Size = new System.Drawing.Size(325, 216);
+            this.commandPanel.TabIndex = 4;
+            this.commandPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.commandPanel_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(940, 352);
+            this.Controls.Add(this.commandPanel);
+            this.Controls.Add(this.graphicPanel);
+            this.Controls.Add(this.textBoxCommand);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.Panel graphicPanel;
+        private System.Windows.Forms.Panel commandPanel;
+
+        public EventHandler Form1_Load { get; private set; }
     }
 }
 
--- ASEproject/Form1.cs
+++ ASEproject/Form1.cs
@@ -12,9 +12,75 @@ namespace ASEproject
 {
     public partial class Form1 : Form
     {
+        Bitmap myBitmap = new Bitmap(640, 480);
+        Boolean mouseDown = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Perform the "Save" action here
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text|*.txt|All Files|*.*";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string textToSave = "This is the text you want to save."; // Replace with your text data
+                    string filePath = saveFileDialog.FileName;
+                    System.IO.File.WriteAllText(filePath, textToSave);
+                    MessageBox.Show("Command saved successfully.", "Save Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button 2 pressed");
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; // get graphics content form
+            g.DrawImageUnscaled(myBitmap, 0, 0); //put the off screen bitmap to the form
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouseDown)
+                return;
+            Graphics g = Graphics.FromImage(myBitmap); //get graphics context off of screen bitmap
+          
+            Refresh(); //system should update the display
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void textBoxCommand_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void graphicPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void commandPanel_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }

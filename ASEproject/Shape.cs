7f0184d91e7df50b52f50f230fafbd094be1f3e3
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-08 17:12:10 +0000

    Abstract class to create shapes such as circles

--- ASEproject/Shape.cs
+++ ASEproject/Shape.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ASEproject
+
 {
-    internal class Shape
+    abstract class MyShape
     {
+        protected Color myColor;
+        protected int myX, myY;
+
+        public abstract void DrawMyShape(Graphics g, Point point);
+        public MyShape(Color color, int x, int y)
+        {
+            this.myColor = color;
+            this.myX = x; this.myY = y;
+           
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + myX + " " + myY;
+        }
     }
 }

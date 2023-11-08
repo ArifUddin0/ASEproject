b8da0f50af21f33dbcdd57ff723449e989f38c68
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-08 18:27:31 +0000

    Added a custom canvas class, 'MyCanvass,' for drawing shapes and managing bitmaps

--- ASEproject/Canvass.cs
+++ ASEproject/Canvass.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASEproject
 {
-    internal class Canvass
+    internal class MyCanvass
     {
+        private Graphics g;
+        private Point p;
+        private Bitmap myBitmap;
+
+        public void DrawMyShape(MyShape shape)
+        {
+            shape.DrawMyShape(g, p);
+        }
+        public void MoveTo(int x, int y)
+        {
+            p = new Point(x, y);    
+        }
+        public MyCanvass(int width, int height)
+        {
+            myBitmap= new Bitmap(width, height);
+            g = Graphics.FromImage(myBitmap);
+        }
+        public Bitmap GetBitmap()
+        {
+            return myBitmap;
+        }
     }
 }

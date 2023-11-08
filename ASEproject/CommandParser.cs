a45e235a43c8105704c2c2d42e0e6ea49bd0891d
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-08 17:07:18 +0000

    Process the commands for circle

--- ASEproject/CommandParser.cs
+++ ASEproject/CommandParser.cs
@@ -5,13 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ASEproject
+
 {
-    internal class CommandParser
+    internal class MyCommandParser
     {
-        public void ProcessCommand(string command, Graphics graphics)
+        public void ProcessCommand(string command, Pen pen, MyCanvass canvas)
         {
-           
+            Console.WriteLine("Processing command: " + command);
+            string[] parts = command.Split(' ');
+
+            if (parts[0] == "circle")
+            {
+                 int radius = Int32.Parse(parts[1]);
+                 MyShape circle = new MyCircle(pen.Color, 15, 15, radius);
+
+                 canvas.DrawMyShape(circle);
+            }
         }
     }
 }

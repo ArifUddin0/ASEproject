eb37923a4bd6365373e15cf243e47e6ab4e912ba
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-10 19:59:27 +0000

    Triangle code has been added in including all 3 sides of the shape and display

--- ASEproject/Triangle.cs
+++ ASEproject/Triangle.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASEproject
 {
-    internal class Triangle
+    internal class MyTriangle:MyShape
     {
+        protected int sideLength;
+       
+
+        public override void DrawMyShape(Graphics myGraphics, Point myPoint)
+        {
+            Pen myPen = new Pen(myColor, 2);
+            Point[] points = new Point[3];
+            points[0] = new Point(myPoint.X, myPoint.Y - sideLength); // Represents the top point of the shape
+            points[1] = new Point(myPoint.X - (sideLength / 2), myPoint.Y + (sideLength / 2)); // Represents the left point of triangle
+            points[2] = new Point(myPoint.X + (sideLength / 2), myPoint.Y + (sideLength / 2)); // Represesnts the right point of traingle
+
+            myGraphics.DrawPolygon(myPen, points);
+        }
+        public MyTriangle(Color myColor, int myX, int myY, int sideLength) : base(myColor, myX, myY)
+        {
+            this.sideLength = sideLength;
+           
+
+        }
     }
 }

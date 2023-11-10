1836000eded13409debe53eeae61c5af4789e020
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-10 19:39:51 +0000

    Rectangle code has been added - giving its height width and abillity to be drawn

--- ASEproject/Rectangle.cs
+++ ASEproject/Rectangle.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASEproject
 {
-    internal class Rectangle
+    internal class MyRectangle:MyShape
     {
+         
+          protected int height;
+          protected int width;
+
+        public override void DrawMyShape(Graphics myGraphics, Point myPoint)
+            {
+                Pen myPen = new Pen(myColor, 2);
+                myGraphics.DrawRectangle(myPen, myPoint.X, myPoint.Y, height, width);
+            }
+          public MyRectangle(Color myColor, int myX, int myY, int height, int width) : base(myColor, myX, myY)
+        {
+            this.height = height;
+            this.width = width;
+                
+        }
+
+
+        }
     }
-}
+

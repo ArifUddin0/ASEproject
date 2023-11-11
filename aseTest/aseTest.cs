8bcc711e80a8bf9dd1d2f18f2456ce620924bb99
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-11 00:00:32 +0000

    Not working taken out for now

--- aseTest/aseTest.cs
+++ aseTest/aseTest.cs
@@ -9,19 +9,7 @@ namespace aseTest
         [TestMethod]
         public void moveto()
         {
-            // Prepare canvas and initialize starting coordinates
-            MyCanvass canvas = new MyCanvass(100, 100);
-
-            // Execute MoveTo method
-            int moveToX = 30, moveToY = 40;
-            canvas.MoveTo(moveToX, moveToY);
-
-            // Access the 'p' property directly from the MyCanvass instance
-            Point currentPoint = canvas.p;
-
-            // Assert that the coordinates have changed
-            Assert.AreEqual(moveToX, currentPoint.X);
-            Assert.AreEqual(moveToY, currentPoint.Y);
+           
         }
     }
 }

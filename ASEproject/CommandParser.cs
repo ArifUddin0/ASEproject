fe7af687b8a82a1d4cad1e073abd13cd557ab275
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-11 23:27:24 +0000

    XML comments for command parser class added in

--- ASEproject/CommandParser.cs
+++ ASEproject/CommandParser.cs
@@ -9,13 +9,22 @@ using System.Threading.Tasks;
 namespace ASEproject
 
 {
+    /// <summary>
+    /// Parses and processes different commands for the project.
+    /// </summary>
     public class MyCommandParser
     {
-       
+        /// <summary>
+        /// Initializes a new instance of the MyCommandParser class.
+        /// </summary>
+        /// <param name="command">The command to be processed.</param>
+        /// <param name="pen">The pen used for drawing.</param>
+        /// <param name="canvas">The canvas where the shapes are drawn.</param>
         public MyCommandParser(string command, Pen pen, MyCanvass canvas)
         {
             Console.WriteLine("Processing command: " + command);
                 string[] parts = command.Split(' ');
+                // Check the first part of the command to determine its type and process accordingly.
 
                 if (parts[0] == "moveto")
                 {
@@ -24,14 +33,17 @@ namespace ASEproject
                     canvas.MoveTo(x, y);
 
                 }
+               // moves the cursor to the selected location
 
                 else if (parts[0] == "circle")
                 {
                     int radius = Int32.Parse(parts[1]);
                     MyShape circle = new MyCircle(pen.Color, 15, 15, radius);
                     canvas.DrawMyShape(circle);
-
+               
                 }
+                // draws a circle on the output.
+
                 else if (parts[0] == "rectangle")
                 {
                 int height = Int32.Parse(parts[1]);
@@ -40,6 +52,7 @@ namespace ASEproject
                 canvas.DrawMyShape(rectangle);
 
                  }
+                // draws a rectangle on the output.
                 else if (parts[0] == "triangle")
                 {
                 int sideLength = Int32.Parse(parts[1]);
@@ -47,22 +60,25 @@ namespace ASEproject
                 canvas.DrawMyShape(triangle);
 
                  }
+                //draws a triangle on the output.
 
                  else if (parts[0] == "clear")
                     {
                     canvas.Clear();
                  }
+                //clears any drawings from the output - fresh sheet.
                 else if (parts[0] == "reset")
                 {
                     canvas.Reset();
                   }
+                //resets the cursor to 0,0.
                 else if (parts[0] == "drawto")
                 {
                     int x = Int32.Parse(parts[1]);
                     int y = Int32.Parse(parts[2]);
                     canvas.DrawTo(x, y);
                  }
-
+                //draws a line from the selected points.
 
         }
     }

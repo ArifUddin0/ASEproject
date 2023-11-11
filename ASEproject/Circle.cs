d11ae04bb5164d19c4df5607318c7879b70be68f
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-11 23:25:32 +0000

    XML comments for circle class added in

--- ASEproject/Circle.cs
+++ ASEproject/Circle.cs
@@ -7,16 +7,34 @@ using System.Threading.Tasks;
 
 namespace ASEproject
 {
-    public class MyCircle : MyShape
+    /// <summary>
+    /// inherits properties from shape.cs
+    /// </summary>
+    public class MyCircle : MyShape 
     {
-       
+       /// <summary>
+       /// radius of circle
+       /// </summary>
         protected int myRadius;
 
+        /// <summary>
+        /// Draws the circle on the graphics context at the specified position.
+        /// </summary>
+        /// <param name="myGraphics">The graphics context to draw on.</param>
+        /// <param name="myPoint">The position to draw the circle.</param>
+      
         public override void DrawMyShape(Graphics myGraphics, Point myPoint)
         {
             Pen myPen = new Pen(myColor, 2);
             myGraphics.DrawEllipse(myPen, myPoint.X, myPoint.Y, myRadius * 2, myRadius * 2);
         }
+        /// <summary>
+        /// Initializes a new instance of the MyCircle class with the specified color, position, and radius.
+        /// </summary>
+        /// <param name="myColor">Color of the circle.</param>
+        /// <param name="myX">X-coordinate of the circle's position.</param>
+        /// <param name="myY">Y-coordinate of the circle's position.</param>
+        ///<param name="myRadius">Radius of the circle.</param>
         public MyCircle(Color myColor, int myX, int myY, int myRadius) : base(myColor, myX, myY)
         {
            this.myRadius = myRadius;

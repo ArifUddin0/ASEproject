3ea0af4f17f9379c945eb210f1e741e4297a14b9
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-12 17:29:12 +0000

    test for circle built and working - checks to see the command follows needed procedure

--- aseTest/aseTest.cs
+++ aseTest/aseTest.cs
@@ -9,7 +9,32 @@ namespace aseTest
         [TestMethod]
         public void moveto()
         {
-           
+            Pen pen = new Pen(Color.Black, 2); // Or another color and size
+            MyCanvass canvas = new MyCanvass(500, 500); // Adjustment of the canvas size
+
+            string command = "moveto 50 50";
+
+            MyCommandParser parser = new MyCommandParser(command, pen, canvas);
+
+            // Assert
+            // Check if the cursor moved to the expected position (50, 50)
+
         }
+
+        [TestMethod]
+        public void Circle_Test()
+        {
+            Pen pen = new Pen(Color.Black, 2); // Or another color and size
+            MyCanvass canvas = new MyCanvass(500, 500); // Adjust the canvas size
+
+            string command = "circle 30";
+
+            MyCommandParser parser = new MyCommandParser(command, pen, canvas);
+
+            // Assert
+            string[] parts = command.Split(' ');
+            Assert.IsTrue(parts.Length == 2 && parts[0] == "circle" && int.TryParse(parts[1], out _));
+        }
+
     }
 }

4e6a82e4d18ca18e843a188281732484b8b13e9c
Arif Uddin <a.uddin2475@student.leedsbeckett.ac.uk>
2023-11-12 18:36:29 +0000

    Test for rectangle has been added in and working along with XML comments

--- aseTest/aseTest.cs
+++ aseTest/aseTest.cs
@@ -6,26 +6,62 @@ namespace aseTest
     [TestClass]
     public class aseTest
     {
-
+        /// <summary>
+        /// Test to confirm that a circle command results in a circle being drawn on the canvas.
+        /// </summary>
         [TestMethod]
-        public void Rectangle_Test()
+        public void Circle()
+
         {
-            Pen pen = new Pen(Color.Black, 2);
-            MyCanvass canvas = new MyCanvass(500, 500);
+            Pen pen = new Pen(Color.Red, 5);
+            MyCanvass canvas = new MyCanvass(350, 300);
 
-            string command = "circle 30";
+            string command = "circle 30"; // example/definiton of a command to draw a circle
 
+            // Execute the circle command
             MyCommandParser parser = new MyCommandParser(command, pen, canvas);
 
             List<string> executedCommands = canvas.GetExecutedCommands();
 
+            // Display the executed commands 
             Console.WriteLine("Executed Commands:");
             foreach (string executedCommand in executedCommands)
             {
                 Console.WriteLine(executedCommand);
             }
 
+            // Asserts that least one  of the commands executed draws a circle
             Assert.IsTrue(executedCommands.Any(command => command.Contains("MyCircle")));
+        }
+
+        /// <summary>
+        /// Test to verify that a rectangle command results in a rectangle being drawn on the canvas.
+        /// </summary>
+        [TestMethod]
+        public void Rectangle_Test()
+        {
+            
+            Pen pen = new Pen(Color.Red, 5);
+            MyCanvass canvas = new MyCanvass(300, 350);
+
+            
+            string command = "rectangle 30 40"; // Define a command to draw a rectangle
+
+            // Executes the rectangle command
+            MyCommandParser parser = new MyCommandParser(command, pen, canvas);
+
+            // Get the list of executed commands
+            List<string> executedCommands = canvas.GetExecutedCommands();
+
+            // Display the executed commands 
+            Console.WriteLine("Executed Commands:");
+            foreach (string executedCommand in executedCommands)
+            {
+                Console.WriteLine(executedCommand);
+            }
+
+            // Assert that at least one command executed draws a rectangle
+            Assert.IsTrue(executedCommands.Any(command => command.Contains("MyRectangle")));
         }
     }
     
